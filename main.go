564004e48a2a0dc15dda877cbf3602ae4020edaf
hugmouse <me@mysh.dev>
2023-07-24 17:49:39 +0200

    Add LICENSE!

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+/*
+Maddy Password Reset - Simple password reset web service for Maddy Mail Server
+Copyright © 2023 Iaroslav Angliuster <me@mysh.dev>, Maddy Password Reset contributors
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
@@ -5,7 +20,6 @@ import (
 	"database/sql"
 	"github.com/akyoto/cache"
 	"github.com/hugmouse/maddy-password-reset/templates"
-	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"html/template"
 	"io"
@@ -67,6 +81,7 @@ const (
 	// HTTPServerPort is an HTTP server port
 	HTTPServerPort = 1323
 
+	// DebugBypassMailSending if true, will not send any emails and will print reset link to the console
 	DebugBypassMailSending = true
 )
 
